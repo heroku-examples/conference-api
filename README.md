436d86a1700033c4f7d4aaca19c0d562c0f8d945
Julian Duque <julianduquej@gmail.com>
2021-11-04 14:59:06 -0400

    add docs & license

---
+++ README.md
@@ -0,0 +1,17 @@
+# conference-api
+
+A REST API for the Conference LWC App
+
+## Install Dependencies
+
+```sh
+npm install
+```
+
+## Run Locally
+
+```sh
+node server.js
+```
+
+
--- server.js
+++ server.js
@@ -1,34 +1,38 @@
+// Load .env config files if present
+require('dotenv').config()
+
 // Require `core` modules and assign them to constants
 // Require the `fs/promises` module, all the methods will return Promises.
-const fs = require('fs/promises');
-const path = require('path');
+const fs = require('fs/promises')
+const path = require('path')
+
 // Require the fastify framework and instantiate it
-const fastify = require('fastify')({ logger: true });
+const fastify = require('fastify')({ logger: true })
 // Define the HTTP Port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 // Define the filename by joining the current directory and data/sessions.json
-const sessionsFilename = path.join(__dirname, 'data', 'sessions.json');
+const sessionsFilename = path.join(__dirname, 'data', 'sessions.json')
 
 // Register CORS Plugin
-fastify.register(require('fastify-cors'));
+fastify.register(require('fastify-cors'))
 
 // Declare the /api/sessions route
 fastify.get('/api/sessions', async (request, reply) => {
   // Read the file using async/await as an `utf8` string
-  const sessionsFile = await fs.readFile(sessionsFilename, 'utf8');
+  const sessionsFile = await fs.readFile(sessionsFilename, 'utf8')
   // Parse the `utf8` string as a JSON object
-  const sessions = JSON.parse(sessionsFile);
+  const sessions = JSON.parse(sessionsFile)
   return sessions
 })
 
 // Run the server!
 async function start () {
   try {
-    await fastify.listen(PORT, '0.0.0.0');
+    await fastify.listen(PORT, '0.0.0.0')
   } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
 }
-start();
+start()
